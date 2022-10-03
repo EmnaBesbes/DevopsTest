5ac5403b4739324501ec715bb5c511db40f37825
EmnaBesbes <emna.besbes@esprit.tn>
2022-10-03 11:49:23 +0100

    initialisation

---
+++ src/main/java/com/example/demo/ProjectTestDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTestDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTestDevopsApplication.class, args);
+	}
+
+}
