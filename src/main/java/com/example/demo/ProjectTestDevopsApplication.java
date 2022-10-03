724d3c26da9e1f24823df99c4bc5f071c1fc1f31
EmnaBesbes <emna.besbes@esprit.tn>
2022-10-03 12:08:47 +0100

    commit 2

--- src/main/java/com/example/demo/ProjectTestDevopsApplication.java
+++ src/main/java/com/example/demo/ProjectTestDevopsApplication.java
@@ -4,7 +4,10 @@ import javax.persistence.Entity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Repository;
 @Entity
+@Repository
+
 @SpringBootApplication
 public class ProjectTestDevopsApplication {
 
